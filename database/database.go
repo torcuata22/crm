01a454ea20b38a76947ff2e3d647948670790bb5
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2025-01-08 22:30:24 -0500

    setting up still

--- database/database.go
+++ database/database.go
@@ -1,1 +1,8 @@
 package database
+
+import (
+	"gorm.io/gorm"
+	//"gorm.io/driver/sqlite" // Sqlite driver based on CGO
+)
+
+var DBConn *gorm.DB
--- main.go
+++ main.go
@@ -1,19 +1,39 @@
 package main
 
 import (
+	"crm/database"
+	"log"
+
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 // setup routes:
 func setupRoutes(app *fiber.App) {
 	app.Get("GetLeads")
+	app.Get("GetLead")
 	app.Post("CreateLead")
-	app.Put("UpdateLead")
+	//app.Put("UpdateLead")
 	app.Delete("DeleteLead")
 }
 
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open(sqlite.Open("leads.db"), &gorm.Config{})
+	if err != nil {
+		// handle the error, e.g., log it or panic
+		log.Fatal(err)
+	}
+}
 func main() {
 	app := fiber.New()
+	initDatabase()
 	setupRoutes(app)
 	app.Listen(":3000")
+	db, err := database.DBConn.DB()
+	if err != nil {
+		// handle error
+	}
+	defer db.Close()
 }
